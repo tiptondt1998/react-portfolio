eb506da25fa76b31668a2a14d9ce28948727cc1a
tiptondt1998 <dylan.tipton@live.com>
2021-03-05 17:01:00 -0500

    added about component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import logo from './logo.svg';
+import './App.css';
+import About from "./components/About";
+function App() {
+  return (
+<div>
+  <About></About>
+</div>
+  );
+}
+
+export default App;
