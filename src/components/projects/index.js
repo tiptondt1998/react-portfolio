10407d8b49001dd30aacee0fd5256d4c25b93aa7
tiptondt1998 <dylan.tipton@live.com>
2021-04-06 12:01:14 -0400

    fixed url to critter

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -7,7 +7,7 @@ function Projects(){
        <div class="nes-container with-title is-centered">
        <p class="title">Projects</p>
       <p>
-      <a href="https://www.crittertails.herokuapp.com">CritterTails</a>      
+      <a href="http://crittertails.herokuapp.com/">CritterTails</a>      
       <br/>
       <a href="http://gentle-hollows-60962.herokuapp.com/login">Doorman</a>
       <br/>
