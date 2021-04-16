36cf293a26cb6a0dfb5d7a54786f52d27a6cb8e5
tiptondt1998 <dylan.tipton@live.com>
2021-04-16 18:03:30 -0400

    added descriptions to each project

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -6,19 +6,25 @@ function Projects(){
       <section>
        <div class="nes-container with-title is-centered">
        <p class="title">Projects</p>
-      <p>
-      <a href="http://crittertails.herokuapp.com/">CritterTails</a>      
+      <p id='projects'>
+      <section id="critter">
+        <a href='http://crittertails.herokuapp.com'>Critter</a> is a social media website for your pets. All you have to do is sign up,
+        and you'll be able to interact with other users as your own pet.
+      </section>
       <br/>
-      <a href="http://gentle-hollows-60962.herokuapp.com/login">Doorman</a>
+      <section id="Doorman">
+        <a href='http://gentle-hollows-60962.herokuapp.com/Login'>Doorman</a> was created with the aim of allowing the COVID-19 policy at 
+        North Knoxville Medical Center to be more consistently enforced. When the nurse assigns a patient to a room, the screener is then
+        able to add a designated visitor to that room. Once the patient has been discharged, the nurse can simply login and remove that patient,
+        along with their only visitor.
+      </section>
       <br/>
-      <img src="../../assets/project_images/gameSearch.JPG"/>
-      <a href="https://tiptondt1998.github.io/GameSearch/">Game Search</a>
-      <br/>
-      <img src="../../assets/project_images/weatherApp.JPG"/>
-      <a href="https://tiptondt1998.github.io/WeatherApp/">Weather App</a>
+      <section id='Game Search'>
+        <a href='https://tiptondt1998.github.io/GameSearch'>GameSearch</a> is a simple application that uses the RAWG API to fetch the top
+        games of a given year. This app will then allow you to view the top 10 games of that year and display them in a grid. Clicking on the
+        displayed games will then provide the rating for the game as well as a description of it.
+      </section>
       </p>
-      <br/>
-
       </div>
       </section>
       );
